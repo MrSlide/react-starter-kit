a3978c5a6c8318345fc5339d669636ee6e0a44c0
Luis Rodrigues <MrSlide@users.noreply.github.com>
2020-04-07 18:28:40 +0100

    feat: Added React Strict Mode HOC

--- src/common/components/app/index.tsx
+++ src/common/components/app/index.tsx
@@ -1,12 +1,12 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, StrictMode } from 'react'
 import GlobalStyles from '../providers/global-styles'
 
 function App (): ReactElement {
   return (
-    <>
+    <StrictMode>
       <GlobalStyles />
       <div>Hello, world!</div>
-    </>
+    </StrictMode>
   )
 }
 
