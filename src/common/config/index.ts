e6d07a6a9fbb8003642f7ddc26aa8251508c7b18
Luis Rodrigues <MrSlide@users.noreply.github.com>
2020-04-13 11:08:15 +0100

    feat(config): Added possibility of providing a default value at runtime

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -2,28 +2,40 @@ import { deepFreeze } from '../utils/object'
 
 const config = deepFreeze(JSON.parse(process.env.CONFIG))
 
+/**
+ * Break down a configuration key string into its parts.
+ * Each part corresponds to a property in an object or
+ * index in an array.
+ *
+ * @param key - The string to tokenize.
+ * @private
+ */
 function tokenizeKey (key: string): string[] {
   return key.split('.')
 }
 
-export function getValue (key: string): any {
+/**
+ * Gets a configuration value associated with the given key.
+ * If no value is available, either `null` or `undefined`,
+ * it is possible to provide a value to default to.
+ *
+ * @param key - The key of the configuration value to get,
+ * @param defaultValue - A value to return if no value is available.
+ * @public
+ */
+export default function get (key: string, defaultValue?: any): any {
   const tokens = tokenizeKey(key)
+  let output = config
 
-  return tokens.reduce(function (acc, token): any {
-    const value = acc[token]
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i]
 
-    if (typeof value === 'undefined') {
-      throw new Error(`The key '${key}' does not exist in the configuration.`)
+    output = output[token]
+
+    if (typeof output === 'undefined' || output === null) {
+      break
     }
-
-    return value
-  }, config)
-}
-
-export default function get (key?: string): any {
-  if (typeof key === 'undefined') {
-    return config
   }
 
-  return getValue(key)
+  return output ?? defaultValue
 }
